10b238334d692fe4a2723d2a373707b213759bd6
Szabolcs Bagoly <szabolcs.bagoly@gmail.com>
2018-05-16 12:12:14 +0300

    fix

--- java/src/com/company/prime/MainClass.java
+++ java/src/com/company/prime/MainClass.java
@@ -8,8 +8,8 @@ import java.util.concurrent.CountDownLatch;
 
 public class MainClass {
 
-    private static final int MAX_NUMBERS = 200;
-    private static final int NO_THREADS = 1;
+    private static final int MAX_NUMBERS = 200000;
+    private static final int NO_THREADS = 3;
     private static final double MEAN_ERROR_THRESHOLD = 0.00008;
 
     public static void main(String[] args) {
